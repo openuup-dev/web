fcd8145115f49f6d83b1d4ce6035d83c7c74376e
Charles <c4a@monocles.eu>
2025-06-07 14:50:27 -0400

    Use a list instead

--- download.php
+++ download.php
@@ -1,12 +1,7 @@
 <?php include("header.php"); ?>
 <h1>Download</h1>
 <p>Below is a table of all of the files in the build you selected. It can take some time to load.</p>
-<table id="download">
-  <tr>
-    <th>Name</th>
-    <th>URL</th>
-  </tr>
-</table>
+<ul id="download"></ul>
 <script>
   (async function() {
     let files = await fetch("./json-api/get.php" +
@@ -15,17 +10,13 @@
     files = files.response.files;
     let names = Object.keys(files);
     names.forEach((name) => {
-      let row = document.createElement("tr");
-      let nameElement = document.createElement("td");
-      nameElement.textContent = name;
-      row.appendChild(nameElement);
-      let url = document.createElement("td");
       let link = document.createElement("a");
-      link.textContent = files[name].url;
       link.href = files[name].url;
-      url.appendChild(link);
-      row.appendChild(url);
-      document.querySelector("#download").appendChild(row);
+      link.textContent = name;
+      let item = document.createElement("li");
+      item.appendChild(link);
+      let downloadElement = document.querySelector("#download");
+      downloadElement.appendChild(item);
     });
   })();
 </script>
